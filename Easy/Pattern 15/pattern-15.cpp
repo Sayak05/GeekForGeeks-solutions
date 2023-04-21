84d27103292256262c11b23376034004e0795cc7
Sayak Sen <121113169+Sayak05@users.noreply.github.com>
2023-04-21 17:35:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 15/pattern-15.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=n-1;i>=0;i--){
+            char s='A';
+            for(int j=0;j<=i;j++){
+                cout<<s;
+                s++;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
