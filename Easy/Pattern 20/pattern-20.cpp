2c447ad209b2f1b42551d1312979ef7028d5bbea
Sayak Sen <121113169+Sayak05@users.noreply.github.com>
2023-04-21 19:05:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 20/pattern-20.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int sp=2*n-2;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<=i;j++){
+                cout<<"*";
+            }
+            for(int j=0;j<sp;j++){
+                cout<<" ";
+            }
+            sp-=2;
+            for(int j=0;j<=i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        int sp2=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                cout<<"*";
+            }
+            for(int j=0;j<sp2;j++){
+                cout<<" ";
+            }
+            sp2+=2;
+            for(int j=0;j<=i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
